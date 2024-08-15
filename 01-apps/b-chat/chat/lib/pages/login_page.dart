bed18e9e2ace2f1869e8fbbcd3e105452f1ca309
Javier Reyes <javierreyes@MacBook-Pro-de-mac.local>
2024-08-15 14:55:49 -0500

    creacion de componente boton

--- 01-apps/b-chat/chat/lib/pages/login_page.dart
+++ 01-apps/b-chat/chat/lib/pages/login_page.dart
@@ -1,3 +1,7 @@
+import 'package:chat/widgets/boton_azul.dart';
+import 'package:chat/widgets/custom_input.dart';
+import 'package:chat/widgets/labels.dart';
+import 'package:chat/widgets/logo.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -6,9 +10,70 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('login Page'),
-      ),
+      backgroundColor: Color.fromARGB(255, 246, 245, 245),
+
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Logo(),
+            _From(),
+            Labels(),
+            
+            Text('Terminos y condiciones', style: TextStyle(fontWeight: FontWeight.w200))
+          ],
+        ),
+      )
     );
   }
 }
+
+
+class _From extends StatefulWidget {
+  //const _From({super.key});
+
+  @override
+  State<_From> createState() => __FromState();
+}
+
+class __FromState extends State<_From> {
+
+  final emailCtrl = TextEditingController();
+  final passCtrl  = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 40),
+      padding: EdgeInsets.symmetric( horizontal: 50),
+
+      child: Column(
+        children: [
+          CustomInput(
+            icon           : Icons.mail_outline, 
+            placeholder    : 'Correo', 
+            keyboardType   : TextInputType.emailAddress,
+            textController : emailCtrl, 
+          ),
+          CustomInput(
+            icon           : Icons.lock, 
+            placeholder    : 'Contraseña', 
+            isPassword     : true,
+            textController : passCtrl, 
+          ),
+
+          BotonAzul(
+            text      : 'Ingrese', 
+            onPressed : (){
+              print('Texto de botón');
+              print('Email: ${emailCtrl.text}');
+              print('Contraseña: ${passCtrl.text}');
+            }
+          )
+        ],
+      ),
+    );
+  }
+
+}
+
---
+++ 01-apps/b-chat/chat/lib/widgets/boton_azul.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class BotonAzul extends StatelessWidget {
+
+  final String text;
+  final void Function() onPressed; 
+
+  const BotonAzul({super.key, required this.text, required this.onPressed});@override
+
+  Widget build(BuildContext context) {
+    return  ElevatedButton(
+      onPressed:  this.onPressed,
+      child: Container(
+        width: double.infinity,
+        height: 55,
+        child: Center(
+          child: Text(this.text, style: TextStyle(color: Colors.white, fontSize: 17) ),
+        ),
+      ), // Texto del botón
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white, 
+        elevation      : 2,
+        padding        : EdgeInsets.symmetric(horizontal: 16),
+      ),
+    );
+  }
+}
+
