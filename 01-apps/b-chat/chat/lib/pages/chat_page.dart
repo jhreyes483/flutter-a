76f3e2d7a1eb58949137b48a3cd2109426112585
Javier Reyes <javierreyes@MacBook-Pro-de-mac.local>
2024-08-15 10:19:32 -0500

    se crean rutas

---
+++ 01-apps/b-chat/chat/lib/pages/chat_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChatPage extends StatelessWidget {
+  //const UsuariosPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Chat Page'),
+      ),
+    );
+  }
+}
