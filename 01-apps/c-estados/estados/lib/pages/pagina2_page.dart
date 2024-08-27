044efa6814b2c57e4b2a1502cfa2bbce3c9ee3d2
Javier Reyes <javierreyes@MacBook-Pro-de-mac.local>
2024-08-27 14:24:29 -0500

    getx, cambiar tema y alertas de getx

--- 01-apps/c-estados/estados/lib/pages/pagina2_page.dart
+++ 01-apps/c-estados/estados/lib/pages/pagina2_page.dart
@@ -29,6 +29,18 @@ class _Pagina2PageState extends State<Pagina2Page> {
               child: Text('Establecer usuario', style: TextStyle(color: Colors.white)),
               color: Colors.blue,
               onPressed: () {
+                Get.snackbar(
+                  'Usuario esablecido', 
+                  'Javier es el nombre del usuario',
+                  backgroundColor: Colors.white,
+                  boxShadows: [
+                    BoxShadow(
+                      color: Colors.black38,
+                      blurRadius: 10
+                    )
+                  ]
+                  ); // alerta
+                  print('termino alerta');
                 usuarioCtrl.cargarUsuario( Usuario(nombre: 'Javier', edad:30) );
               }
             ),
@@ -46,6 +58,13 @@ class _Pagina2PageState extends State<Pagina2Page> {
               color: Colors.blue,
               onPressed: () {
                 usuarioCtrl.agregarProfesion('Test profesion ${usuarioCtrl.profesionesCount + 1}');
+              }   
+            ),
+            MaterialButton( 
+              child: Text('Cambiar tema', style: TextStyle(color: Colors.white)),
+              color: Colors.blue,
+              onPressed: () {
+                Get.changeTheme( Get.isDarkMode ? ThemeData.light() : ThemeData.dark() ); // cambio de tema
               }   
             ),
           ],
