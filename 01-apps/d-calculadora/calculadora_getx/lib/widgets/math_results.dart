3a1794fcf7d13116f417cf3a13e6edbfde1b4ce1
Javier Reyes <javierreyes@MacBook-Pro-de-mac.local>
2024-08-27 15:52:01 -0500

    se agrega proyecto calculadora getx

---
+++ 01-apps/d-calculadora/calculadora_getx/lib/widgets/math_results.dart
@@ -0,0 +1,27 @@
+import 'package:calculadora_getx/controllers/calculator_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'sub_result.dart';
+import 'line_separator.dart';
+import 'main_result.dart';
+
+class MathResults extends StatelessWidget {
+  
+  final calculatorCtrl = Get.find<CalculatorController>(); // busca la estancia de el controller en el contex
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Obx(  // re dibuja cuando hay algun cambio 
+      () => Column(
+        children: [
+          SubResult( text: '${ calculatorCtrl.firstNumber }' ),
+          SubResult( text: '${ calculatorCtrl.operation }' ),
+          SubResult( text: '${ calculatorCtrl.secondNumber }' ),
+          LineSeparator(),
+          MainResultText( text: '${ calculatorCtrl.mathResult }' ),
+        ],
+    ));
+  }
+}
