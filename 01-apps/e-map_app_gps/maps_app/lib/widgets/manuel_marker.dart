9d7c1d9cad1f6ba5bbd449abc6bd38c1be58a44e
jhreyes483 <jhreyes483@misena.edu.co>
2024-09-03 10:02:00 -0500

    maquetacion de boton (confirmar destino) y animacion 233

--- 01-apps/e-map_app_gps/maps_app/lib/widgets/manuel_marker.dart
+++ 01-apps/e-map_app_gps/maps_app/lib/widgets/manuel_marker.dart
@@ -6,24 +6,78 @@ class ManuelMarker extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
+    /* iciono de seleccionar ubicacion */
     final size = MediaQuery.of(context).size; // adapta a toda la pantalla 
     return SizedBox( // similar al container
       width: size.width,
       height: size.height,
       child: Stack( // sirve para colocar un widget encima de otro
         children: [
+            const Positioned( // posiciona el widgwt segun el top y left
+              top: 70,
+              left: 20,
+              child: _BtnBack()
+            ),
+
+
           Transform.translate( // sube un poco en el offset
             offset: const Offset(0,-22),
+
+          
             child: Center(
               child: BounceInDown( // animacion para que caiga el icono
                 from: 100,
-                child:  Icon(Icons.location_on_rounded, size: 50)
+                child:const Icon(Icons.location_on_rounded, size: 50)
                 ),
             ),
+          ),
+
+          /* Boton de confirmar */
+          Positioned(
+            bottom: 70,
+            left: 40,
+            child: FadeInUp( // animacion
+              duration: const Duration(milliseconds: 300),
+              child: MaterialButton(
+                minWidth: size.width -120,
+                child: const Text('Confirmar destino', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w300)),
+                color: Colors.black,
+                elevation: 0,
+                height:40,
+                shape: const StadiumBorder(), // redondea los bordes del boton
+                onPressed: () {
+                  // TODO confirmar ubicacion
+                },
+              ),
+            )
           )
+
+
         ],
       )
+    );
+  }
+}
+
+class _BtnBack extends StatelessWidget {
+  const _BtnBack({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeInLeft( // animacion
+    duration: const Duration(microseconds: 300),
+      child: CircleAvatar(
+        maxRadius: 30,
+        backgroundColor: Colors.white,
+        child: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new, color: Colors.black),
+          onPressed: () {
+            // TODO: cancelar el marcador manual
+          },
+          ),
+      ),
     );
   }
 }
