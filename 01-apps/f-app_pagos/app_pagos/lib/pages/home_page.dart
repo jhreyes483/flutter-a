288040c63e99f1561f772495f183535fa5405a41
Javier Reyes <javierreyes@MacBook-Pro-de-mac.local>
2024-09-06 15:38:13 -0500

    diseño de tarjeta en home page

--- 01-apps/f-app_pagos/app_pagos/lib/pages/home_page.dart
+++ 01-apps/f-app_pagos/app_pagos/lib/pages/home_page.dart
@@ -1,17 +1,56 @@
+import 'package:app_pagos/data/tarjetas.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size; //dimenciones de la pantalla
+
     return Scaffold(
       appBar: AppBar (
         title: Text('Pagar'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: (){}
+          )
+        ],
       ),
-      body: Center(
-        child: Text('Home page'),
-      ),
+      body: Stack(
+        children: [
+
+          Positioned( // Centrar en el eje X
+            width: size.width,
+            height: size.height,
+            top:200,
+
+            child: PageView.builder(
+              controller: PageController(
+                viewportFraction: 0.9 /* correo hacia un lado para que se vea el borde la la otra tarjeta */
+              ),
+              physics: const BouncingScrollPhysics(),
+            
+              itemCount: tarjetas.length,
+              itemBuilder: (_, i){
+                final tarjeta = tarjetas[i];
+                return CreditCardWidget( // paquete importado
+                  cardNumber: tarjeta.cardNumberHidden, 
+                  expiryDate: tarjeta.expiracyDate,
+                  cardHolderName: tarjeta.cardHolderName,
+                  cvvCode: tarjeta.cvv,
+                  showBackView: false,
+                  onCreditCardWidgetChange: (CreditCardBrand ) { 
+            
+                    },
+                  );
+              }),
+          )
+        ],
+      )
     );
   }
 }
